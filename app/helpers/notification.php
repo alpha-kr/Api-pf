13391e55c9fd45613526005225fadca5f4018789
unknown <bandy@uninorte.edu.co>
2020-05-08 10:55:13 -0500

    notificaciones agregadas entrega casi final!

---
+++ app/helpers/notification.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\helpers;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging\Notification;
+use Kreait\Firebase\Messaging\AndroidConfig;
+ class  NotificationFB {
+    public static function enviar($title,$body,$img=null, $token)
+    {
+
+
+
+        $firebase  = (new Factory)->withServiceAccount(__DIR__.'/firebasekey.json');
+        $notification = Notification::fromArray([
+            'title' => $title,
+            'body' => $body,
+            'image' => $img
+        ]);
+        $config = AndroidConfig::fromArray([
+            'ttl' => '3600s',
+            'priority' => 'normal',
+            'notification' => [
+                'title' => $title,
+            'body' => $body,
+            'image' => $img,
+                'color' => '#0D0F74',
+            ],
+        ]);
+        $messaging = $firebase->createMessaging();
+        $message = CloudMessage::withTarget('token',$token)
+            ->withNotification($notification)
+            ->withData(['score' => '1.0']);
+            $message= $message->withAndroidConfig($config);
+            $messaging->send($message);
+    }
+
+}
+?>
